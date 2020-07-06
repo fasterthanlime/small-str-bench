d1270321e36c76e7030ae757ac60611bdf3254a0
Amos Wenger <amoswenger@gmail.com>
2020-07-06 10:38:20 +0200

    from/into/clone benchmarks

--- benches/string_bench.rs
+++ benches/string_bench.rs
@@ -5,10 +5,10 @@ use criterion::{
 };
 use smartstring::{LazyCompact, SmartString};
 use smol_str::SmolStr;
-use std::{sync::Arc, time::Duration};
+use std::time::Duration;
 
-const FAST_AND_INACCURATE: bool = true;
-const SIZES: &[usize] = &[3, 5, 8, 15, 24, 50, 120, 230, 550];
+const FAST_AND_INACCURATE: bool = false;
+const SIZES: &[usize] = &[5, 10, 20, 200, 600, 1800, 3500];
 
 trait SizeGroup {
     fn size_group<F: Fn(&mut BenchmarkGroup<WallTime>, usize)>(&mut self, name: &str, f: F);
@@ -18,8 +18,8 @@ impl SizeGroup for Criterion {
     fn size_group<F: Fn(&mut BenchmarkGroup<WallTime>, usize)>(&mut self, name: &str, f: F) {
         let mut group = self.benchmark_group(name);
         if FAST_AND_INACCURATE {
-            group.measurement_time(Duration::from_millis(200));
-            group.warm_up_time(Duration::from_millis(50));
+            group.measurement_time(Duration::from_millis(800));
+            group.warm_up_time(Duration::from_millis(200));
         }
 
         for &n in SIZES {
@@ -28,11 +28,45 @@ impl SizeGroup for Criterion {
     }
 }
 
-trait BenchClone {
+trait FromStr {
+    fn from_str(s: &str) -> Self;
+}
+
+impl FromStr for String {
+    fn from_str(s: &str) -> Self {
+        Self::from(s)
+    }
+}
+
+impl FromStr for SmolStr {
+    fn from_str(s: &str) -> Self {
+        Self::from(s)
+    }
+}
+
+impl FromStr for SmartString<LazyCompact> {
+    fn from_str(s: &str) -> Self {
+        Self::from(s)
+    }
+}
+
+trait Benches {
+    fn bench_from<S: FromStr>(&mut self, name: &str, n: usize);
+    fn bench_into<S: From<String> + Into<String>>(&mut self, name: &str, n: usize);
     fn bench_clone<S: From<String> + Clone>(&mut self, name: &str, n: usize);
 }
 
-impl<M: Measurement> BenchClone for BenchmarkGroup<'_, M> {
+impl<M: Measurement> Benches for BenchmarkGroup<'_, M> {
+    fn bench_from<'a, S: FromStr>(&mut self, name: &str, n: usize) {
+        self.bench_with_input(BenchmarkId::new(name, n), &n, |b, n| {
+            b.iter_batched(
+                || -> String { make_string(*n) },
+                |s| S::from_str(&s),
+                BatchSize::SmallInput,
+            )
+        });
+    }
+
     fn bench_clone<S: From<String> + Clone>(&mut self, name: &str, n: usize) {
         self.bench_with_input(BenchmarkId::new(name, n), &n, |b, n| {
             b.iter_batched(
@@ -42,14 +76,33 @@ impl<M: Measurement> BenchClone for BenchmarkGroup<'_, M> {
             )
         });
     }
+
+    fn bench_into<S: From<String> + Into<String>>(&mut self, name: &str, n: usize) {
+        self.bench_with_input(BenchmarkId::new(name, n), &n, |b, n| {
+            b.iter_batched(
+                || -> S { make_string(*n).into() },
+                |s| -> String { s.into() },
+                BatchSize::SmallInput,
+            )
+        });
+    }
 }
 
 fn benchmark(c: &mut Criterion) {
+    c.size_group("from", |g, n| {
+        g.bench_from::<String>("string", n);
+        g.bench_from::<SmolStr>("smol", n);
+        g.bench_from::<SmartString<LazyCompact>>("smart", n);
+    });
     c.size_group("clone", |g, n| {
         g.bench_clone::<String>("string", n);
-        g.bench_clone::<Arc<String>>("arc", n);
         g.bench_clone::<SmolStr>("smol", n);
         g.bench_clone::<SmartString<LazyCompact>>("smart", n);
+    });
+    c.size_group("into", |g, n| {
+        g.bench_into::<String>("string", n);
+        g.bench_into::<SmolStr>("smol", n);
+        g.bench_into::<SmartString<LazyCompact>>("smart", n);
     });
 }
 
